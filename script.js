85843c5b8e8f2539483769a2f88f8a01a42863d3
omtamang <119476805+omtamang@users.noreply.github.com>
2024-05-06 18:52:06 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var button = document.getElementById("quote_button");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+// Show an alert message when the button is clicked
+alert("Message sent!");
+});}
